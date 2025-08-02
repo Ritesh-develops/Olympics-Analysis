c7a085cbb9f56b6191a7dde4d8c2b2ed2f96d3c6
Ritesh jha <rjamasterstudios@gmail.com>
2025-08-02 20:06:22 +0530

    dropdown of year and country added

--- app.py
+++ app.py
@@ -6,12 +6,27 @@ df = pd.read_csv("athlete_events.csv")
 region_df = pd.read_csv("noc_regions.csv")
 
 data = preprocessor.preprocessor(df, region_df)
-medal_tally=helper.Medal_tally(data)
 
+st.sidebar.header("Olympics Analysis")
 user_menu = st.sidebar.radio(
     "Select an Option",
     {"Medal Tally", "Overall Analysis", "Country-Wise Analysis", "Athlete wise Analysis"}
 )
 
 if user_menu == "Medal Tally":
-    st.dataframe(medal_tally)
+    st.sidebar.header("Medals Tally")
+    years, country = helper.country_year_list(data)
+    selected_year = st.sidebar.selectbox("Select Year", years)
+    selected_country = st.sidebar.selectbox("Select Year", country)
+    medal_tally=helper.fetch_medal_tally(data, selected_year, selected_country)
+
+    if selected_year == "Overall" and selected_country == "Overall":
+        st.header("Overall Medals Tally")
+    if selected_year != "Overall" and selected_country == "Overall":
+        st.header("Medal Tally in " + str(selected_year) + " Olympics")
+    if selected_year == "Overall" and selected_country != "Overall":
+        st.header("Medal Tally of " + selected_country)
+    if selected_year != "Overall" and selected_country != "Overall":
+        st.header("Medal Tally of "+ selected_country + " in the year "+ str(selected_year))
+
+    st.table(medal_tally)
--- helper.py
+++ helper.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import numpy as np
 
 def Medal_tally(df):
     medal_tally = df.drop_duplicates(subset=["Team", "NOC", "Games", "Year", "City", "Sport", "Event", "Medal"])
@@ -9,3 +9,38 @@ def Medal_tally(df):
     medal_tally["Bronze"] = medal_tally["Bronze"].astype("int")
     medal_tally["Silver"] = medal_tally["Silver"].astype("int")
     return medal_tally
+
+def country_year_list(df):
+    years = df["Year"].unique().tolist()
+    years.sort()
+    years.insert(0, "Overall")
+
+    country = np.unique(df['region'].dropna().values).tolist()
+    country.sort()
+    country.insert(0, "Overall")
+
+    return years, country
+
+def fetch_medal_tally(df, year, country):
+  medal_df = df.drop_duplicates(subset=["Team", "NOC", "Games", "Year", "City", "Sport", "Event", "Medal"])
+  flag = 0
+  if year=="Overall" and country == "Overall":
+    temp_df = medal_df
+  if year == "Overall" and country!="Overall":
+    flag = 1
+    temp_df = medal_df[medal_df["region"]== country]
+  if year != "Overall" and country=="Overall":
+    temp_df = medal_df[medal_df["Year"]== int(year)]
+  if year != "Overall" and country!="Overall":
+    temp_df = medal_df[(medal_df["region"]== country) & (medal_df["Year"]==year)]
+  
+  if flag == 1:
+    x = temp_df.groupby('Year').sum()[['Gold', 'Silver','Bronze']].sort_values('Year').reset_index()
+  else:
+    x = temp_df.groupby('region').sum()[['Gold', 'Silver','Bronze']].sort_values('Gold', ascending=False).reset_index()
+  x["Total"] = x["Gold"]+x["Silver"]+x["Bronze"]
+  x["Gold"] = x["Gold"].astype("int")
+  x["Bronze"] = x["Bronze"].astype("int")
+  x["Silver"] = x["Silver"].astype("int")
+
+  return x
